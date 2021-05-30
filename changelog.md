3b294d9078b28fa06e3b77e24cd3c884a4ae1bdd
SAMUEL REIS <samuel.reis@extreme.digital>
2021-05-30 02:45:49 -0300

    atualização do docker file e das credencias do gservice

---
+++ changelog.md
@@ -0,0 +1,5 @@
+# Relatório de alterações
+
+| **Versão** | **Feature** | **Data** |
+| ---------- | ------------|----------|
+| v1.0 | build massive annouces an deal events sign up | 28/04/2021 |
--- src/main/java/br/bot/hermodr/Config/SheetServiceConnection.java
+++ src/main/java/br/bot/hermodr/Config/SheetServiceConnection.java
@@ -1,19 +1,15 @@
 package br.bot.hermodr.Config;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.List;
@@ -22,14 +18,14 @@ public class SheetServiceConnection {
     private Sheets sheetsService;
     private static String APPLICATION_NAME = "hermodr";
     private static String SPREEDSHEET_ID = "1c7tbmvtLcWoOIr1cZ2X2kQ_6Cz-rN3wT-sD3ldTPB20";
-    private static String CREDENTIALS_FILE_PATH = "/credentials.json";
+    private static String CREDENTIALS_FILE_PATH = "/gsheet-service.json";
     private static GsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    //private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
     private static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS);
 
     private static Credential authorize() throws IOException, GeneralSecurityException {
-        // Load client secrets.
+        /*// Load client secrets.
         InputStream in = SheetServiceConnection.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
                 JSON_FACTORY, new InputStreamReader(in));
@@ -43,6 +39,16 @@ public class SheetServiceConnection {
 
         Credential credential = new AuthorizationCodeInstalledApp(
                 flow, new LocalServerReceiver()).authorize("user");
+
+        return credential;*/
+
+        InputStream in = SheetServiceConnection.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+
+        Credential credential = GoogleCredential.fromStream(in)
+                .createScoped(SCOPES);
 
         return credential;
     }
